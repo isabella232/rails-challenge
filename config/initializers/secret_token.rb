90f3d00f46fe3d75d7b175b08475ded1de76b68d
Ideais <ideais@caxambu.ideais.local>
2014-11-04 16:53:23 -0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Samplerb::Application.config.secret_token = 'ec993068cb21048d59607a6f5101bf9c586a07918190deb9fb5c32c9f90ab31014de39fb7d74dc7b60d7fb6ab2aab8763f251e65538a702d81ac3abf453e94ee'
